0bf2061d7f9ca3cebe1a74d01ba580a5b4e7a07f
James Weis <jweis@revampagency.com>
2017-09-08 20:58:15 -0400

    added conditional if no background image provided - does not break app

--- src/Book.js
+++ src/Book.js
@@ -18,7 +18,15 @@ class Book extends Component {
       <li key={book.id}>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+            {book.imageLinks ? 
+            <div className="book-cover" style={{ width: 128, height: 193, 
+                backgroundImage: `url(${book.imageLinks.thumbnail})` }}>    
+            </div>
+              :
+              <div className="book-cover" style={{ width: 128, height: 193, 
+                backgroundImage: `url(../imgs/No_Cover.jpg)` }}>    
+            </div>
+            }
             <div className="book-shelf-changer">
             <form>
               <select value={this.state.shelf} onChange={this.handleChange}>
@@ -32,7 +40,7 @@ class Book extends Component {
             </div>
           </div>
           <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.author}</div>
+          <div className="book-authors">{book.authors}</div>
         </div>
       </li>
     )
